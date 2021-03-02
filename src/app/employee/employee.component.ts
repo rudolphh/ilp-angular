08c969c589758743c0fcb77aa9c45e0ae0001a37
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-03-02 09:02:42 -0800

    fixed Module 2 - assignment 2

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -6,22 +6,43 @@ import { Employee } from './employee';
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent implements OnInit {
+export class EmployeeComponent implements OnInit{
 
-  employee : Employee = {};
+  public employee : Employee = {
+    employeeId: 1,
+    firstName: "Rudy",
+    lastName: "Hernandez",
+    dept: "ilp",
+    city: "modesto",
+    email: "rudyah@gmail.com"
+  };
+
+  public emp : Employee = {}
+
+  editMode : boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-
+    throw new Error('Method not implemented.');
   }
 
   EditEmp(): void {
-    
+    this.emp.employeeId = this.employee.employeeId;
+    this.emp.firstName = this.employee.firstName;
+    this.emp.lastName = this.employee.lastName;
+    this.emp.dept = this.employee.dept;
+    this.emp.city = this.employee.city;
+    this.emp.email = this.employee.email;
   }
 
   UpdateEmp(): void {
-
+    this.employee.employeeId = this.emp.employeeId;
+    this.employee.firstName = this.emp.firstName;
+    this.employee.lastName = this.emp.lastName;
+    this.employee.dept = this.emp.dept;
+    this.employee.city = this.emp.city;
+    this.employee.email = this.emp.email;
   }
 
 }
